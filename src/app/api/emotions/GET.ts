61c59a61a5e1475c6bf816e9f2d9bb6675f5c5bd
Yeison Molina <yeison.molina91@gmail.com>
2024-07-17 22:00:13 -0500

    Add endpoint to get emotions

--- src/app/api/emotions/GET.ts
+++ src/app/api/emotions/GET.ts
@@ -1,7 +1,28 @@
 import { NextResponse } from 'next/server'
+import '@/domain/polyfills'
+
+import { apiMiddleware, responseApiException, responseApiSuccess } from '@/domain/providers/http'
+import { getEmotions } from '@/domain/actions/emotions'
 
 export async function GET(req: Request) {
   const res = NextResponse
-  console.log(req)
-  return res.json({})
+
+  try {
+    await apiMiddleware(req, {}, res, {
+      authorization: true,
+      only: ['GET'],
+    })
+
+    const emotions = await getEmotions()
+
+    return responseApiSuccess(res, {
+      content: {
+        message: 'Get emotions successfully!',
+        emotions,
+      },
+    })
+  } catch (exception) {
+    console.error(exception)
+    return responseApiException(res, exception)
+  }
 }
